8b08b5073420982691a703f5cb8142e8ad844963
aaron <alkrauss48@gmail.com>
2020-12-09 10:54:14 -0600

    Add init script for registering a bot

---
+++ config.js
@@ -0,0 +1,17 @@
+const company = 'Clevyr'
+const title = 'Dev'
+
+const config = {
+  users: [
+    {
+      firstName: 'Aaron',
+      lastName: 'Bot',
+      email: 'aaron@botbotbot.com',
+      password: 'password',
+      company,
+      title
+    }
+  ]
+}
+
+module.exports = config
