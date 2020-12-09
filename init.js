7cbab027921cc4af50e813bc427c6c700926ea34
aaron <alkrauss48@gmail.com>
2020-12-09 11:42:24 -0600

    Add 5 users for browser testing

--- init.js
+++ init.js
@@ -1,55 +1,41 @@
 const puppeteer = require('puppeteer')
 const config = require('./config')
 
-const REGISTER_URL = 'https://dev.onelambda.clevyr.on-livi.com/register';
+const { users } = config
+const REGISTER_URL = 'https://dev.onelambda.clevyr.on-livi.com/register'
 
-(async () => {
-  const browser = await puppeteer.launch({
-    headless: false
-  })
-  const page = await browser.newPage()
-  await page.setViewport({
-    width: 1000,
-    height: 800,
-    deviceScaleFactor: 1
-  })
+users.forEach((user) => {
+  (async () => {
+    const browser = await puppeteer.launch({
+      headless: false
+    })
+    const page = await browser.newPage()
+    await page.setViewport({
+      width: 1000,
+      height: 800,
+      deviceScaleFactor: 1
+    })
 
-  const { users } = config
-  const user = users[0]
+    await page.goto(REGISTER_URL, { waitUntil: 'networkidle2' })
 
-  await page.goto(REGISTER_URL, { waitUntil: 'networkidle2' })
+    await page.type('input[placeholder^="First"]', user.firstName)
+    await page.type('input[placeholder^="Last"]', user.lastName)
+    await page.type('input[placeholder^="Email"]', user.email)
+    await page.type('input[placeholder^="Password"]', user.password)
+    await page.type('input[placeholder^="Re-enter"]', user.password)
+    await page.type('input[placeholder^="Company"]', user.company)
+    await page.type('input[placeholder^="Title"]', user.title)
+    await page.click('[class^="Checkbox__StyledCheckbox"]')
 
-  await page.type('input[placeholder^="First"]', user.firstName)
-  await page.type('input[placeholder^="Last"]', user.lastName)
-  await page.type('input[placeholder^="Email"]', user.email)
-  await page.type('input[placeholder^="Password"]', user.password)
-  await page.type('input[placeholder^="Re-enter"]', user.password)
-  await page.type('input[placeholder^="Company"]', user.company)
-  await page.type('input[placeholder^="Title"]', user.title)
-  await page.click('[class^="Checkbox__StyledCheckbox"]')
+    await page.click('form [type="submit"]')
 
-  await page.click('form [type="submit"]')
+    await page.waitForNavigation({ waitUntil: 'networkidle2' })
 
-  await page.waitForNavigation({ waitUntil: 'networkidle2' })
+    await page.waitForTimeout(5000)
+    await page.click('button') // 'I am ready' avatar selection button
 
-  await page.waitForTimeout(5000)
-  await page.click('button') // 'I am ready' avatar selection button
+    await page.waitForTimeout(5000)
 
-  await page.waitForTimeout(12000)
-
-  // Skip through the "Get Started" navigation
-  await page.click('[class^="Modal"] [class^="CloseButton"]')
-  await page.waitForTimeout(100)
-  await page.click('[class^="Modal"] [class^="CloseButton"]')
-  await page.waitForTimeout(100)
-  await page.click('[class^="Modal"] [class^="CloseButton"]')
-  await page.waitForTimeout(100)
-
-  await page.keyboard.down('ArrowUp')
-
-  await page.waitForTimeout(1000000)
-
-  await page.screenshot({ path: 'example.png' })
-
-  await browser.close()
-})()
+    await browser.close()
+  })()
+})
